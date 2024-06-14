48fa9bf30a97d132b334920e6ac41c6a80f19545
somT-oss <somtochukwuuchegbu@gmail.com>
2024-06-14 11:34:18 +0100

    SCRUM-40-build-out-sign-in-api-company update: postgresdb connection to prod

--- reconciliation_backend/settings.py
+++ reconciliation_backend/settings.py
@@ -117,10 +117,10 @@ WSGI_APPLICATION = 'reconciliation_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
+        'NAME': os.getenv("POSTGRES_NAME"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
         'PORT': os.getenv("DB_PORT"),
         'TEST': {
             'NAME': 'testdb',
