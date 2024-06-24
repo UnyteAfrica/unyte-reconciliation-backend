64305e0c1d3b5b63b0f35657f87179b046417e92
somT-oss <somtochukwuuchegbu@gmail.com>
2024-06-24 10:28:14 +0100

    SCRUM-161-refactor-user-model-for-authentication fix: cors error

--- reconciliation_backend/settings/base.py
+++ reconciliation_backend/settings/base.py
@@ -47,6 +47,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
