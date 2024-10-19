b9743229089d7bf49abe16fe525115a4467cbfdb
Isaac Adewumi <ienioladewumi@gmail.com>
2024-10-19 10:25:28 +0100

    fix merchants signup (#108)

--- agents/tests.py
+++ agents/tests.py
@@ -10,23 +10,23 @@ import urllib.parse
 
 class AgentAppTestCase(TestCase):
     AGENT_DICT = {
-        "first_name": 'John',
-        "last_name": "Doe",
-        "middle_name": "",
-        "email": "agent_email@gmail.com",
-        "password": "testing321",
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'middle_name': '',
+        'email': 'agent_email@gmail.com',
+        'password': 'testing321',
         'home_address': '10 john doe lane',
-        "bank_account": "123456789",
-        "bvn": "12345678901",
+        'bank_account': '123456789',
+        'bvn': '12345678901',
     }
     INSURER_DICT = {
-            'business_name': 'Unyte',
-            'admin_name': 'unyte_admin',
-            'business_registration_number': '12345678',
-            'email': 'testing321@gmail.com',
-            'password': 'password'
+        'business_name': 'Unyte',
+        'admin_name': 'unyte_admin',
+        'business_registration_number': '12345678',
+        'email': 'testing321@gmail.com',
+        'password': 'password',
     }
-    
+
     def setUp(self) -> None:
         insurer_route = reverse('insurer:register_insurer')
 
@@ -43,15 +43,15 @@ class AgentAppTestCase(TestCase):
     def test_create_agent(self) -> None:
         route = reverse('agents:register_agent')
         data = {
-            "first_name": self.AGENT_DICT.get('first_name'),
-            "last_name": self.AGENT_DICT.get('last_name'),
-            "email": self.AGENT_DICT.get('email'),
-            "home_address": self.AGENT_DICT.get('home_address'),
-            "bank_account": self.AGENT_DICT.get('bank_account'),
-            "bvn": self.AGENT_DICT.get('bvn')
+            'first_name': self.AGENT_DICT.get('first_name'),
+            'last_name': self.AGENT_DICT.get('last_name'),
+            'email': self.AGENT_DICT.get('email'),
+            'home_address': self.AGENT_DICT.get('home_address'),
+            'bank_account': self.AGENT_DICT.get('bank_account'),
+            'bvn': self.AGENT_DICT.get('bvn'),
         }
 
-        user = get_object_or_404(CustomUser, email=self.INSURER_DICT.get('email')) 
+        user = get_object_or_404(CustomUser, email=self.INSURER_DICT.get('email'))
         insurer = get_object_or_404(Insurer, user=user)
-        insurer_uuid = insurer.unyte_unique_insurer_id               
+        insurer_uuid = insurer.unyte_unique_insurer_id
         print(route + '?' + urllib.parse.urlencode({'invite': insurer_uuid}))
